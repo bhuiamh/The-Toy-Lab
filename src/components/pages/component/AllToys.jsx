1e4935a360c29132b26919e65d6315c109223dd2
bhuiamhe <bhuiamh@gmail.com>
2023-05-21 01:50:59 +0600

    AllToys are Loaded

--- src/components/pages/component/AllToys.jsx
+++ src/components/pages/component/AllToys.jsx
@@ -3,6 +3,9 @@ import Toy from "./Toy";
 
 const AllToys = () => {
   const [toys, setToys] = useState([]);
+  const [displayedToys, setDisplayedToys] = useState([]);
+  const [showAllToys, setShowAllToys] = useState(false);
+  const toysPerPage = 6;
 
   useEffect(() => {
     fetch("http://localhost:5000/alltoys")
@@ -10,12 +13,46 @@ const AllToys = () => {
       .then((data) => setToys(data));
   }, []);
 
+  useEffect(() => {
+    if (showAllToys) {
+      setDisplayedToys(toys);
+    } else {
+      setDisplayedToys(toys.slice(0, toysPerPage));
+    }
+  }, [toys, showAllToys]);
+
+  const handleViewMore = () => {
+    setShowAllToys(true);
+  };
+
+  const handleShowLess = () => {
+    setShowAllToys(false);
+  };
+
   return (
     <div className="mx-5 px-5 my-10 place-content-center">
       <div className="grid grid-cols-1 md:grid-cols-3">
-        {toys.map((toy) => (
-          <Toy key={toy._id} toy={toy}></Toy>
+        {displayedToys.map((toy) => (
+          <Toy key={toy._id} toy={toy} />
         ))}
+      </div>
+      <div className="flex justify-center">
+        {!showAllToys && toys.length > toysPerPage && (
+          <button
+            onClick={handleViewMore}
+            className="bg-blue-500 text-white px-4 py-2 rounded-md mt-4 hover:bg-blue-600 focus:outline-none"
+          >
+            View More
+          </button>
+        )}
+        {showAllToys && (
+          <button
+            onClick={handleShowLess}
+            className="bg-blue-500 text-white px-4 py-2 rounded-md mt-4 hover:bg-blue-600 focus:outline-none"
+          >
+            Show Less
+          </button>
+        )}
       </div>
     </div>
   );
--- src/components/pages/component/Toy.jsx
+++ src/components/pages/component/Toy.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import StarRatings from "react-star-ratings";
 
 const Toy = ({ toy }) => {
   const {
@@ -25,13 +26,29 @@ const Toy = ({ toy }) => {
         </p>
         <p>{subCategory}</p>
         <div className="grid grid-cols-3">
-          <p className="text-lg font-bold text-green-500">${price}</p>
-          <p>{ratings}</p>
-          <p>{availableQuantity}</p>
+          <p className="text-lg font-bold text-green-700">${price}</p>
+          <p>
+            <StarRatings
+              rating={ratings}
+              starRatedColor="green"
+              starEmptyColor="lightgray"
+              numberOfStars={5}
+              starDimension="15px"
+              starSpacing="2px"
+            />
+          </p>
+          <div className="indicator">
+            <span className="indicator-item badge badge-secondary font-bold bg-pink-500">
+              {availableQuantity}+
+            </span>
+            <p className="px-2 py-0 bg-green-700 rounded font-bold">
+              Available
+            </p>
+          </div>
         </div>
 
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
+        <div className="card-actions justify-end ">
+          <button className="btn btn-primary w-full mt-2">Details</button>
         </div>
       </div>
     </div>
